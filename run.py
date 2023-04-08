fe5a12a4b94d75bac9c6f95641d0b8a2655ff065
fr00tbat <fruitbat@fruitbat.ie>
2023-04-08 13:58:38 +0000

    Add move_to and destination script functions

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ location = "pickling room"
 
 
 # storage & display areas
-fridge = [[],[],[]]
+fridge = [[], [], []]
 pickling_table = []
 
 # food items and combined ingredient arrays
@@ -28,7 +28,8 @@ class Jar:
         self.lid = "on"
 
     def describe(self):
-        return f"description: \nsize: {self.size}\nlabel: {self.label}\ncontents: {self.contents}\nlid: {self.lid}"
+        description = f"description: \nsize: {self.size}\nlabel: {self.label}\ncontents: {self.contents}\nlid: {self.lid}"
+        return description
 
     def open():
         self.lid = "off"
@@ -45,11 +46,71 @@ class Cucumber:
     def __init__(self):
         self.pickledeness = "fresh"
         self.crunch = "very crunchy"
-    
+
     def describe(self):
-        return f"description: \npickledness: {self.pickledeness}\ncrunch: {self.crunch}"
+        description = f"description: \npickledness: {self.pickledeness}\ncrunch: {self.crunch}"
+        return description
 
 
+def move_to(destination):
+    """
+    move around the room
+    """
+    if destination == "pickling room":
+        # run pickling room option script
+        location = "pickling room"
+        consider_pickling_room()
+    elif destination == "fridge":
+        # run fridge option script
+        location = "fridge"
+        consider_fridge()
+    elif destination == "pickling table":
+        # run pickling table option script
+        location = "pickling table"
+        consider_pickling_table()
+    else:
+        print("Where are you?")
+        print("You have floated away")
+        print("> Move back to pickling room")
+
+
+def consider_pickling_room():
+    print("You are inside the pickling room.")
+    print("What would you like to do?")
+    print("(enter an option)")
+    print("1. View pickling table")
+    print("2. View fridge")    
+
+
+def consider_fridge():
+    print("You crack open your gently humming fridge.")
+    print("What would you like to do?")
+    print("(enter an option)")
+    print("1. Survey items on fridge shelves")
+    print("2. Eat a pickle")
+    print("n. Close fridge")
+
+
+def consider_pickling_table():
+    print("You approach the pickling table.")
+    print("What would you like to do?")
+    print("(enter an option)")
+
+    print("1. Survey pickling table items")
+    print("2. Pick out an empty jar")
+    print("3. Put out a bowl")
+    print("4. Put out ingredients")
+
+    print("5. Wash item in sink")
+    print("6. Boil an item for 15m in hot water")
+    print("")
+    print("")
+    print("n. Move across the pickling room")
+
+
+"""
 cucumber1 = Cucumber()
 print(cucumber1.describe())
+"""
 
+move_to("pickling table")
